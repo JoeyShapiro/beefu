9776b3dc683e2c14fa651bcbc6508c3eec7164f8
JoeyShapiro <joeyashapiro@gmail.com>
2025-05-25 15:50:17 -0500

    added settings

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,33 @@ use std::io::{Read, Seek};
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
+    let mut delay = 0;
+    let mut mem_size = 30_000;
+    let mut i = 0;
+    while i < args.len() {
+        if args[i] == "--ops" {
+            let ops = args[i + 1].parse::<f32>().expect("Unable to parse ops");
+            if ops > 0.0 {
+                delay = (1.0 / ops * 1000.0) as u64;
+            }
+            i += 1;
+        } else if args[i] == "--mem" {
+            mem_size = args[i+1].parse::<usize>().expect("Unable to parse mem");
+            if mem_size < 1 {
+                panic!("Memory size must be greater than 0");
+            }
+            i += 1;
+        }
+
+        i += 1;
+    }
+
     // open file
     let file = std::fs::File::open(&args[1]).expect("Unable to open file");
     // read file
     let mut reader = std::io::BufReader::new(file);
 
-    let mut memory = vec![0_u8; 30_000];
+    let mut memory = vec![0_u8; mem_size];
     let mut pointer = 0;
     let mut stack = Vec::new();
     let mut pc = 0_u64;
@@ -68,5 +89,8 @@ fn main() {
         }
 
         pc += 1;
+        if delay > 0 {
+            std::thread::sleep(std::time::Duration::from_millis(delay));
+        }
     }
 }
